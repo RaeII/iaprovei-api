2937b4f78cef244eeb728fe34b18469d5277eea2
LCSSchmidt <lucas_miguel_schmidt@hotmail.com>
2025-07-29 17:31:12 -0300

    [Feat] Enable cors for all methods

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,12 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   dotenv.config({ path: join(__dirname, '..', '.env') });
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  });
   app.setGlobalPrefix('api');
   patchNestJsSwagger();
   const config = new DocumentBuilder().setTitle('API').setDescription('API description').setVersion('1.0').addBearerAuth().build();
